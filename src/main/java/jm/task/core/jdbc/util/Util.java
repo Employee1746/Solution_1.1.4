374178e104fdebd0b02e32a52c717897bfd60b6c
Malh1n <58389533+Malh1n@users.noreply.github.com>
2021-05-21 00:51:16 +0300

    Решение на проверку

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
     private final static String URL = "jdbc:mysql://localhost:3306/users";
     private final static String USERNAME = "root";
     private final static String PASSWORD = "g23LHmVqZYZ8";
