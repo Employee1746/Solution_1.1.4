eebf19b676fa1f0a871a772ecde9f57a0da3684d
Malh1n <58389533+Malh1n@users.noreply.github.com>
2021-06-02 20:08:05 +0300

    getSessionFactory

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -18,23 +18,23 @@ public class Util {
     private static SessionFactory sessionFactory;
 
     private Util() {
-        Configuration configuration = new Configuration();
-        Properties settings = new Properties();
-        settings.put(Environment.URL, URL);
-        settings.put(Environment.USER, USERNAME);
-        settings.put(Environment.PASS, PASSWORD);
-        settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-        settings.put(Environment.SHOW_SQL, "true");
-        configuration.setProperties(settings);
-        configuration.addAnnotatedClass(User.class);
-        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().
-                applySettings(configuration.getProperties()).build();
-        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
     }
 
     public static SessionFactory getSessionFactory() {
         if (instance == null) {
             instance = new Util();
+            Configuration configuration = new Configuration();
+            Properties settings = new Properties();
+            settings.put(Environment.URL, URL);
+            settings.put(Environment.USER, USERNAME);
+            settings.put(Environment.PASS, PASSWORD);
+            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            settings.put(Environment.SHOW_SQL, "true");
+            configuration.setProperties(settings);
+            configuration.addAnnotatedClass(User.class);
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().
+                    applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         }
         return sessionFactory;
     }
